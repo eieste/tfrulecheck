86588a443fe54940352fda7385dfa9916b92177f
Stefan Eiermann <stefan.eiermann@eneka.de>
2024-11-11 07:47:21 +0000

    fix: Version handling

--- setup.py
+++ setup.py
@@ -1,18 +1,12 @@
 # -*- coding: utf-8 -*-
 from setuptools import find_packages, setup
 
-f = open("VERSION", "r")
-VERSION = f.read()
-f.close()
-
-
 f = open("README.md", "r")
 LONG_DESCRIPTION = f.read()
 f.close()
 
 setup(
     name="tfutility",
-    version=VERSION,
     description="TfUtility provides different tools for Terraform Developers",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
